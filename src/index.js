e06c0e326c3122ca6f696021b32c653a0c0a1cdf
tomasduoc-14 <to.caceress@duocuc.cl>
2026-05-13 00:16:39 -0400

    Change server port from 3000 to 8080

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getProductos } = require('./database');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
